41629a3aa4ea3f0d4380e2dc8ee69809492231f2
Gordon Maclean <maclean@ucar.edu>
2014-11-25 10:12:34 -0700

    add commented setcap command

--- runserver.sh
+++ runserver.sh
@@ -2,6 +2,8 @@
 
 # nohup python manage.py runserver 0.0.0.0:8000 >& /tmp/django.log &
 
+# sudo setcap 'cap_net_bind_service=+ep' /usr/bin/python3.4
+
 if ! pgrep -f "memcached -s /tmp/django_memcached.sock" > /dev/null; then
     memcached -s /tmp/django_memcached.sock -d
 fi
