2dcee4139c966167874b9ab7c2969c2c43243af5
Gordon Maclean <maclean@users.noreply.github.com>
2014-12-16 16:02:11 -0700

    empty url redirects to ncharts

--- datavis/urls.py
+++ datavis/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
@@ -8,7 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'datavis.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^ncharts/', include('ncharts.urls', namespace='ncharts')),
-    url(r'^admin/', include(admin.site.urls), name="admin:index"),
-    url(r'^$', 'datavis.views.index')
+    url(r'^$', RedirectView.as_view(url='/ncharts')),
+
+    url(r'^ncharts/', include('ncharts.urls', namespace='ncharts'), name="ncharts"),
+    url(r'^admin/', include(admin.site.urls), name="admin:index")
 )
