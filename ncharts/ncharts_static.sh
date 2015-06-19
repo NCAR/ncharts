1a91dfff6f863c0f43fbd43719cde1eb58286e82
Gordon Maclean <maclean@users.noreply.github.com>
2015-06-19 12:37:34 -0600

    Updated script that downloads static files, and some src= urls in base.html

--- ncharts/ncharts_static.sh
+++ ncharts/ncharts_static.sh
@@ -7,6 +7,8 @@ dest=$(readlink -f $dest)
 dest=$dest/ncharts/static/ncharts
 
 [ -d $dest ] || mkdir -p $dest
+[ -d $dest/js ] || mkdir -p $dest/js
+[ -d $dest/js/modules ] || mkdir -p $dest/js/modules
 
 echo "Downloading and copying static files to $dest"
 
@@ -14,40 +16,50 @@ tmpdir=$(mktemp -d /tmp/ncharts_static_XXXXXX)
 trap "{ rm -rf $tmpdir; }" EXIT
 
 cd $tmpdir
-bower install jquery
-bower install bootstrap
+bower install jquery || exit 1
+bower install bootstrap || exit 1
+bower install moment || exit 1
+bower install moment-timezone || exit 1
 
-rsync -av bower_components/jquery/dist/ $dest/js
-rsync -av bower_components/bootstrap/dist/ $dest
+rsync -av bower_components/jquery/dist/ $dest/js || exit 1
+rsync -av bower_components/bootstrap/dist/ $dest || exit 1
+rsync -av bower_components/moment/min/moment.min.js $dest/js || exit 1
+rsync -av bower_components/moment-timezone/builds/moment-timezone-with-data.min.js $dest/js || exit 1
 
-# Highcharts. These bower commands don't get what we want.
-# bower install https://github.com/highslide-software/highcharts.com.git
-# bower install https://github.com/highslide-software/highstock-release.git
+# This gets git://github.com/robdodson/highcharts.com.git, which I'm not sure I want
+# bower install highstock
+# We'll get what we want from highcharts.com with wget
 
-# instead download zip files
-# www.highcharts/com/download
-wget code.highcharts.com/zips/Highcharts-4.0.4.zip
-wget code.highcharts.com/zips/Highstock-2.0.4.zip
 
-# only difference between Highcharts and Highstock:
-# Only in highcharts/js: highcharts-all.js
-# Only in highcharts/js: highcharts.js
-# Only in highcharts/js: highcharts.src.js
-# Only in highstock/js: highstock.js
-# Only in highstock/js: highstock.src.js
+# Either can get the full highcharts zip files, or get just what we want.
+# For now, just get what we want.
+do_zip=false
+if $do_zip; then
 
-mkdir highcharts
-cd highcharts
+    # download zip files
+    # www.highcharts/com/download
+    wget code.highcharts.com/zips/Highcharts.zip || exit 1
+    wget code.highcharts.com/zips/Highstock.zip || exit 1
+    mkdir highcharts
+    cd highcharts
 
-unzip ../Highcharts-*.zip > /dev/null
-rsync -av js $dest
+    unzip ../Highcharts.zip > /dev/null
+    rsync -av js $dest || exit 1
 
-cd -
+    cd -
 
-mkdir highstock
-cd highstock
+    mkdir highstock
+    cd highstock
 
-unzip ../Highstock-*.zip > /dev/null
-rsync -av js/highstock*.js $dest/js
+    unzip ../Highstock.zip > /dev/null
+    rsync -av js/highstock*.js $dest/js || exit 1
+    cd -
+else
+    wget code.highcharts.com/stock/highstock.js || exit 1
+    wget code.highcharts.com/stock/modules/exporting.js || exit 1
+    wget code.highcharts.com/stock/modules/heatmap.js || exit 1
+    rsync -av highstock.js $dest/js || exit 1
+    rsync -av exporting.js $dest/js/modules || exit 1
+    rsync -av heatmap.js $dest/js/modules || exit 1
+fi
 
-cd -
