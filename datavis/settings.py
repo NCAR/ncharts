8420aa7cae1699ad56cfdfe2f7d3c592679d84f2
Gordon Maclean <maclean@ucar.edu>
2015-07-17 12:44:06 -0600

    Added TEMPLATES to settings, to be able to use messages framework

--- datavis/settings.py
+++ datavis/settings.py
@@ -21,8 +21,6 @@ SECRET_KEY = '6-u3&i0wz1lgpwlq3apf1)&o%niv4gql4iv_ibr2^^e2y#=_=6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 if DEBUG:
     VAR_RUN_DIR = BASE_DIR
@@ -67,6 +65,30 @@ MIDDLEWARE_CLASSES = (
     'datavis.middleware.InternalUseOnlyMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
+
+# TEMPLATE_DEBUG = True
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 ROOT_URLCONF = 'datavis.urls'
 
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DJVIRT=${DJVIRT:$HOME/virtualenvs/django}
+DJVIRT=${DJVIRT:-$HOME/virtualenvs/django}
 [ $VIRTUAL_ENV ] || source $DJVIRT/bin/activate
 
 python3 manage.py test ncharts/tests
