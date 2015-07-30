245594abdf12caf74853ad50c01a51b37e25805f
Hien Nguyen <hnguyen@ucar.edu>
2015-07-30 13:30:15 -0600

    Combined the tabs with few variables

--- ncharts/ncharts/models.py
+++ ncharts/ncharts/models.py
@@ -277,8 +277,28 @@ class Dataset(models.Model):
 
         tab_dict = OrderedDict(sorted(tab_dict.items()))
        
-        for tab in tab_dic:
+        combined_vars = []
+        combined_tabs = []
+        temp_tab = tab_dict
+        tab_limit = 5
+        for i, tab in enumerate(temp_tab):
             tab_dict[tab].sort(key=lambda x: x.lower())
+            combined_vars += tab_dict[tab]
+            combined_tabs.append(tab)
+            if len(combined_vars) > tab_limit and len(combined_tabs) == 1:
+                combined_vars = []
+                del combined_tabs[:]
+            elif len(combined_vars) > tab_limit or (len(combined_vars) <= tab_limit and i == (len(temp_tab) - 1)):
+                for toremove in combined_tabs:
+                    tab_dict = {key: value for key, value in tab_dict.items() if key != toremove}
+                new_tab = combined_tabs[0] + "-" + combined_tabs[-1]
+                tab_dict[new_tab] = combined_vars
+                combined_vars = []
+                del combined_tabs[:]
+         
+        temp_tab.clear()
+         
+        tab_dict = OrderedDict(sorted(tab_dict.items()))
 
         return tab_dict
 
