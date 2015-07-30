7100d74f608c049cc76c35f1493c645e7e5dcd2a
Gordon Maclean <maclean@users.noreply.github.com>
2015-07-30 10:45:22 -0600

    List the existing sessions

---
+++ ncharts/ncharts/management/commands/list_sessions.py
@@ -0,0 +1,22 @@
+from django.core.management.base import NoArgsCommand
+
+from ncharts.models import ClientState
+from ncharts import views as nc_views
+
+from django.contrib.sessions.models import Session
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+
+        sessions = Session.objects.all()
+        print("#sessions=%d" % len(sessions))
+
+        for sess in sessions:
+            sess_dict = sess.get_decoded()
+            print("session keys=%s" % (repr([k for k in sess_dict.keys()])))
+
+            for sessk in sess_dict:
+                if len(sessk) > 5 and sessk[0:5] == "pdid_":
+                    print("session, sessk=%s" % (sessk))
+
+
