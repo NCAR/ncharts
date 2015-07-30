bff64793ddf3952d17640e0c19ce34f57b39dac3
Gordon Maclean <maclean@users.noreply.github.com>
2015-07-30 10:17:15 -0600

    Removed last_save from ClientState

--- ncharts/ncharts/management/commands/clear_clients.py
+++ ncharts/ncharts/management/commands/clear_clients.py
@@ -1,16 +1,44 @@
 from django.core.management.base import NoArgsCommand
 
 from ncharts.models import ClientState
-import datetime, pytz
+from ncharts import views as nc_views
+
+from django.contrib.sessions.models import Session
+
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
 
-        old = datetime.date.today() - datetime.timedelta(days=14)
-        aged = ClientState.objects.filter(last_save__lt=old)
 
-        for clnt in aged:
-            print("deleting, pk=%d, dataset=%s, last_save=%s" % \
-                (clnt.pk, clnt.dataset, clnt.last_save))
-            clnt.delete()
+        clnts = ClientState.objects.all()
+
+        sessions = Session.objects.all()
+        print("#sessions=%d" % len(sessions))
+
+        active = set()
+        ndeleted = 0
+
+        for sess in sessions:
+            sess_dict = sess.get_decoded()
+
+            for clnt in clnts:
+                dset = clnt.dataset
+                project = dset.project
+                cid_name = nc_views.client_id_name(
+                    project.name, dset.name)
+
+                if cid_name in sess_dict and sess_dict[cid_name] == clnt.pk:
+                    active.add(clnt.pk)
+                    break
+        for clnt in clnts:
+            if clnt.pk in active:
+                print("client found in session: pk=%d, dataset=%s" % \
+                    (clnt.pk, clnt.dataset))
+            else:
+                print("client not found in session: pk=%d, dataset=%s, deleting" % \
+                    (clnt.pk, clnt.dataset))
+                clnt.delete()
+                ndeleted += 1
+
+        print("#clients=%d, #deleted=%d" % (len(clnts), ndeleted))
 
--- ncharts/ncharts/management/commands/list_clients.py
+++ ncharts/ncharts/management/commands/list_clients.py
@@ -1,14 +1,42 @@
 from django.core.management.base import NoArgsCommand
 
 from ncharts.models import ClientState
-import datetime, pytz
+from ncharts import views as nc_views
+
+from django.contrib.sessions.models import Session
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
 
         clnts = ClientState.objects.all()
 
+        sessions = Session.objects.all()
+        print("#sessions=%d" % len(sessions))
+
+        active = set()
+        n_inactive = 0
+
+        for sess in sessions:
+            sess_dict = sess.get_decoded()
+
+            for clnt in clnts:
+                dset = clnt.dataset
+                project = dset.project
+                cid_name = nc_views.client_id_name(
+                    project.name, dset.name)
+
+                if cid_name in sess_dict and sess_dict[cid_name] == clnt.pk:
+                    active.add(clnt.pk)
+                    break
         for clnt in clnts:
-            print("pk=%d, dataset=%s, last_save=%s" % \
-                (clnt.pk, clnt.dataset, clnt.last_save))
+            if clnt.pk in active:
+                print("client found in session: pk=%d, dataset=%s" % \
+                    (clnt.pk, clnt.dataset))
+            else:
+                print("client not found in session: pk=%d, dataset=%s" % \
+                    (clnt.pk, clnt.dataset))
+                n_inactive += 1
+
+        print("#clients=%d, #in-active=%d" % (len(clnts),n_inactive))
+
 
