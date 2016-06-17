3d8f14882d75a3d4e46ef8b2016aa1d23b4e8c0f
Erik Johnson <ej@ucar.edu>
2016-06-17 17:44:53 -0600

    datavis.settings.production: raise ValueError if EOL_DATAVIS_SECRET_KEY env var is not populated; #10

--- datavis/settings/production.py
+++ datavis/settings/production.py
@@ -9,3 +9,8 @@ DEBUG = False
 LOG_DIR     = '/var/log/django'
 VAR_RUN_DIR = '/var/run/django'
 VAR_LIB_DIR = '/var/lib/django'
+
+SECRET_KEY = os.environ.get('EOL_DATAVIS_SECRET_KEY')
+
+if SECRET_KEY == None:
+  raise ValueError('EOL_DATAVIS_SECRET_KEY environment variable must be set when running with datavis.settings.production')
