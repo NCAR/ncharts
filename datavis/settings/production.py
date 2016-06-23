5eb6b0333b5dac96c85682c56be8d7e04339fb0f
Erik Johnson <ej@ucar.edu>
2016-06-23 11:45:06 -0600

    production settings: update filenames of logging handlers if LOG_DIR has changed; #10

--- datavis/settings/production.py
+++ datavis/settings/production.py
@@ -6,6 +6,8 @@ from .default import *
 
 DEBUG = False
 
+DEFAULT_LOG_DIR = LOG_DIR
+
 LOG_DIR     = '/var/log/django'
 VAR_RUN_DIR = '/var/run/django'
 VAR_LIB_DIR = '/var/lib/django'
@@ -32,3 +34,13 @@ CACHE_MIDDLEWARE_KEY_PREFIX = ''
 # TODO: decrease logging levels below DEBUG in production
 #       https://docs.djangoproject.com/en/1.8/topics/logging/
 #
+
+if LOG_DIR != DEFAULT_LOG_DIR:
+
+  #
+  # iterate over LOGGING['handlers'] and update filenames w/ new LOG_DIR
+  #
+
+  for key, value in LOGGING['handlers'].items():
+    if 'filename' in value:
+      value['filename'] = value['filename'].replace(DEFAULT_LOG_DIR, LOG_DIR)
