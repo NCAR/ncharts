2a6282ab1d3e78e4c0e38278442485b2652d9c7b
Erik Johnson <ej@ucar.edu>
2016-10-18 15:27:25 -0600

    add convenience file to be sourced to load datavis env vars; #15

---
+++ docker/datavis-vars.sh
@@ -0,0 +1,10 @@
+#
+# ABOUT
+#   env vars for datavis.eol.ucar.edu and datavis-dev
+#
+# USAGE
+#   source this file:
+#     $ . docker/datavis-vars.sh
+
+export DATAVIS_LOG_DIR=/var/log/django
+export DATAVIS_DB_DIR=/var/lib/django
