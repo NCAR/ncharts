6c2ae9236178a60899109119b264669e336be929
Gary Granger <granger@ucar.edu>
2018-01-04 12:12:58 -0700

    DJANGO_SETTINGS_MODULE needs to be set also apparently

--- datavis-clear.sh
+++ datavis-clear.sh
@@ -5,6 +5,7 @@
 keyfile=/etc/systemd/system/httpd.service.d/datavis-secret-key.conf 
 cd /var/django/ncharts
 source ../virtualenv/django/bin/activate
+export DJANGO_SETTINGS_MODULE=datavis.settings.production
 eval export `grep EOL_DATAVIS_SECRET_KEY $keyfile | sed -e 's/^.*\"EOL/EOL/' -e 's/\"$//'`
 # echo $EOL_DATAVIS_SECRET_KEY
 ./manage.py clearsessions
