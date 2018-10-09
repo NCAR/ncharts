bca7f06ae62c72bac4afb4f361bb657a2724b510
Gordon Maclean <maclean@users.noreply.github.com>
2018-10-09 16:09:56 -0600

    Clean unreferenced VariableTimes

--- ncharts/management/commands/clear_clients.py
+++ ncharts/management/commands/clear_clients.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 
+from ncharts.models import VariableTimes
 from ncharts.models import ClientState
 from ncharts import views as nc_views
 
@@ -10,11 +11,16 @@ class Command(BaseCommand):
     def handle(self, **options):
 
         clnts = ClientState.objects.all()
+        print("#clnts=%d" % len(clnts))
 
         sessions = Session.objects.all()
         print("#sessions=%d" % len(sessions))
 
+        vtimes = VariableTimes.objects.all()
+        print("#vtimes=%d" % len(vtimes))
+
         active = set()
+        dtimes_active = set()
         ndeleted = 0
 
         for sess in sessions:
@@ -35,6 +41,11 @@ class Command(BaseCommand):
             if clnt.pk in active:
                 print("client found in session: pk=%d, dataset=%s" % \
                     (clnt.pk, clnt.dataset))
+                # dtimes = clnt.data_times.all()
+                # for dt in dtimes:
+                #     print("client pk=%d, active data_time, type(dt)=%s, dt.pk=%d" % \
+                #             (clnt.pk, type(dt), dt.pk))
+                dtimes_active.update(clnt.data_times.all())
             else:
                 print("client not found in session: pk=%d, dataset=%s, deleting" % \
                     (clnt.pk, clnt.dataset))
@@ -42,4 +53,14 @@ class Command(BaseCommand):
                 ndeleted += 1
 
         print("#clients=%d, #deleted=%d" % (len(clnts), ndeleted))
+
+        ndeleted = 0
+        for vt in vtimes:
+            # print("type vt=%s" % type(vt))
+            if vt not in dtimes_active:
+                print("Variable time not found in a client: pk=%d, deleting" % \
+                        vt.pk)
+                vt.delete()
+                ndeleted += 1
+        print("#vtimes=%d, #deleted=%d" % (len(vtimes), ndeleted))
 
