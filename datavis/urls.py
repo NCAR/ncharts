334989e26467cafd144cff394ebccd2e64f22f3c
Gordon Maclean <maclean@users.noreply.github.com>
2018-10-16 19:59:01 -0600

    Re-order entries in urls.py, most likely first

--- datavis/urls.py
+++ datavis/urls.py
@@ -8,6 +8,11 @@ admin.autodiscover()
 
 urlpatterns = [
 
+
+    # If URL starts with ncharts, remove "ncharts/", pass what's
+    # left on to ncharts.urls
+    url(r'^ncharts/', include('ncharts.urls', namespace='ncharts'), name="ncharts"),
+
     # redirect empty URL to /ncharts
     url(r'^$', RedirectView.as_view(url='/ncharts',permanent=True)),
 
@@ -15,9 +20,6 @@ urlpatterns = [
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
         content_type='text/plain')),
 
-    # If URL starts with ncharts, remove "ncharts/", pass what's
-    # left on to ncharts.urls
-    url(r'^ncharts/', include('ncharts.urls', namespace='ncharts'), name="ncharts"),
-
     url(r'^admin/', include(admin.site.urls), name="admin:index")
+
 ]
